ee167ae6cd876193443eb7c48f894026d2de1aa3
ameliaamanatulislam <ameliaamanatulislam@gmail.com>
2026-09-05 13:18:01 +0700

    Projek produk baru

---
+++ produk/src/main/java/com/amel/produk/entity/Produk.java
@@ -0,0 +1,34 @@
+package com.amel.produk.entity;
+
+public class Produk {
+    private int id;
+    private String nama;
+    private double harga;
+    private String deskripsi;
+
+    public Produk() {
+    }
+
+    public Produk(int id, String nama, double harga, String deskripsi) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+        this.deskripsi = deskripsi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+}
